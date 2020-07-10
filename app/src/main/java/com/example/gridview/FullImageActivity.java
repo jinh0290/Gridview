e78905f3d66a4a18a4bc558b42006c0a56ea97f0
jinh0290 <wlsgur0810@gmail.com>
2020-07-10 20:29:00 +0900

    0

---
+++ app/src/main/java/com/example/gridview/FullImageActivity.java
@@ -0,0 +1,26 @@
+package com.example.gridview;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+
+/**
+ * Created by tutlane on 24-08-2017.
+ */
+public class FullImageActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.image_details);
+        // Get intent data
+        Intent i = getIntent();
+        // Get Selected Image Id
+        int position = i.getExtras().getInt("id");
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+        ImageView imageView = (ImageView) findViewById(R.id.full_image_view);
+        imageView.setImageResource(imageAdapter.thumbImages[position]);
+    }
+}
+
+
---
+++ app/src/main/java/com/example/gridview/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.gridview;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GridView gv = (GridView) findViewById(R.id.gridview);
+        gv.setAdapter(new ImageAdapter(this));
+    }
+}
+
